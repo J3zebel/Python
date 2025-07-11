6745e146a384fe8ae5246af83b82034ef1d20da0
J3zebel <155735738+J3zebel@users.noreply.github.com>
2025-07-11 12:56:17 +0530

    st

--- Star.py
+++ Star.py
@@ -1,13 +1,16 @@
-n = int(input("Enter a number: ")) 
-for i in range(1, n+1):          
-    print("* " * i)
+n = int(input("Enter no. of rows:"))
+for i in range (n+1):
+     for j in range(i):
+          print("*",end=" ")
+     print()
 
+m="-------------------"
+print(m)
 
-n = int(input("Enter a number:"))
-for i in range(n+1):
-    print("",end="")
+n = int(input("Enter no. of rows:"))
+for i in range (n):
     for j in range(i,n):
-        print(" ",end="")
-    for j in range(i):
-        print("*",end=" ")
+          print("",end=" ")
+    for j in range(i+1):
+          print("*",end=" ")
     print()
