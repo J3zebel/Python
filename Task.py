49170c00690c36fd245ed6cd50af993ce08b8443
J3zebel <155735738+J3zebel@users.noreply.github.com>
2025-07-11 12:57:52 +0530

    ts

---
+++ Task.py
@@ -0,0 +1,104 @@
+n=5
+m="-----------------"                       #5 * in 1 line
+print("*"*n)
+print(m)
+
+
+
+n=5
+for i in range (n):
+    for j in range (n):                         #5 * IN 5 LINE
+        print("*", end=" ")
+    print()
+print(m)
+
+
+
+n=5
+for i in range (n):
+    for j in range (i+1):
+        print("*", end=" ")                 #right triangle
+    print()
+print(m)
+
+
+n=5
+for i in range (n):
+    for j in range (i,n):
+        print("*", end=" ")                 #inverted right triangle
+    print()
+print(m)
+
+n=5
+for i in range (n):
+    for j in range(i,n):
+        print(" ",end=" ")               # left triangle
+    for j in range(i+1):                
+        print("*",end=" ")    
+    print()
+print(m)
+
+
+n=5
+for i in range(n):
+    for j in range(i):
+        print(" ",end=" ")                  #inverted left triangle
+    for j in range(i,n):
+        print("*",end=" ") 
+    print()
+print(m)
+
+
+n=5
+for i in range(n):
+    for j in range (i,n):
+        print(" ",end=" ")
+    for j in range (i):                         # Hill pattern
+        print("*",end=" ")
+    for j in range(i+1):
+            print("*",end=" ")
+    print()
+print(m)
+
+
+n=5
+for i in range (n):
+    for j in range(i):
+        print(" ",end=" ")
+    for j in range(i,n-1):                      #Inverted Hill pattern
+        print("*",end=" ")
+    for j in range (i,n):
+        print("*",end=" ")
+    print()
+print(m)
+
+
+n=5
+for i in range(n):
+    for j in range(i,n):
+        print(" ",end=" ")                      #pyramid
+    for j in range(i):
+        print("*",end="   ")
+    print()
+print(m)
+
+
+n=5
+for i in range (n-1):
+    for j in range(i,n):
+        print(" ",end=" ")
+    for j in range (i):
+        print("*",end=" ")
+    for j in range (i+1):
+        print("*",end=" ")
+    print()
+for i in range (n):                         #diamond
+    for j in range(i+1):
+        print(" ",end=" ")
+    for j in range(i,n-1):
+        print("*",end=" ")
+    for j in range(i,n):
+        print("*",end=" ")
+    print()
+
+
